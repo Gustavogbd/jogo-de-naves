d188cfbfb9ba82987ec677914433bf0459f52ccf
Gustavo <gustavokkg2@hotmail.com>
2021-12-04 18:03:01 -0300

    cria função das teclas WSD para movimentação e disparo

--- js/main.js
+++ js/main.js
@@ -6,15 +6,47 @@ function start(){
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
-    var jogo = {}
+    //Principais variáveis do jogo
+    var jogo = {};
+    
+    var TECLA = {
+        W: 87,
+        S: 83,
+        D: 68
+    };
+    jogo.pressionou = [];
+    //verifica se o usuário pressionou alguma tecla
+
+    $(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    });
+
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+    //loop
     jogo.timer = setInterval(loop,30);
     function loop(){
         movefundo();
-    }
+        movejogador();
+    };
 
     function movefundo(){
         esquerda = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position",esquerda-1);
-    }
+    };
 
-}
+    function movejogador(){
+        if (jogo.pressionou[TECLA.W]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo-10);
+        }
+        if (jogo.pressionou[TECLA.S]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",topo+10);
+        }
+        if (jogo.pressionou[TECLA.D]){
+            //chama função disparo
+        }
+    }
+};
