ee3c54bf7a37a25fa84925ebb1e0443b4d2a50e2
Gustavo <gustavokkg2@hotmail.com>
2021-12-04 23:24:36 -0300

    funções de movimento para inimigo2 e amigo

--- js/main.js
+++ js/main.js
@@ -32,6 +32,8 @@ function start(){
         movefundo();
         movejogador();
         moveinimigo1();
+        moveinimigo2();
+        moveamigo();
     };
 
     function movefundo(){
@@ -68,6 +70,22 @@ function start(){
             posicaoY = parseInt(Math.random() * 334);
             $("#inimigo1").css("left",694);
             $("#inimigo1").css("top",posicaoY);
+        }
+    }
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left",posicaoX-3);
+        if (posicaoX<=0) {
+            $("#inimigo2").css("left",755);
+        }
+    }
+
+    function moveamigo() {
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left",posicaoX+1);
+        if (posicaoX>906) {
+            $("#amigo").css("left",0);
         }
     }
 };
